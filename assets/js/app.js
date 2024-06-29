573102c792a4ebc8f312b098dd6fae344d7966d7
Brenda Rosas <bsrosasm@gmail.com>
2024-06-29 16:20:14 -0600

    subi los cambios que realice de los archivos

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,21 @@ const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+// se declaro la funcion como asincrona
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data= response.json;    // de aqui no estoy segura si se debia poner data, porque no lo estoy pasando a json
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`; //de aqui segun yo estaban mal declarados los valores 
+  $b.textContent = `${data.blog}`;
+  $l.textContent =  `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // no estoy segura de aqui pero creo que era $n
 }
 
 displayUser('stolinski').catch(handleError);
